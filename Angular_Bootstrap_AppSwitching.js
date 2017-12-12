4dba1d3e8fbfefecac638e66347cc6429ded8209
Vishal Kumar Sahu <viishalkumarsahu@gmail.com>
2017-12-12 11:11:16 +0530

    Update Angular_Bootstrap_AppSwitching.js

--- Angular_Bootstrap_AppSwitching.js
+++ Angular_Bootstrap_AppSwitching.js
@@ -1,3 +1,13 @@
+<section data-application="seg2" id="tpg-view">
+<!-- <section data-ng-app="seg2" id="tpg-view"> -->
+    <div class="container" data-ng-controller="TaxProGoController">
+        <div id="tpg-views" data-ui-view>Fetching App</div>
+    </div>
+    <div data-ng-show='preloader' class="preloader"></div>
+</section>
+
+
+
 function initAngular(app_name){
     var _el = "[data-application='"+app_name+"']";
     var dom_el = $(_el).get(0);
