5cc10ceabc4aab6b34e1704bbe8abaaa122441e3
Vishal Kumar Sahu <viishalkumarsahu@gmail.com>
2017-12-12 11:17:50 +0530

    Update Angular_Bootstrap_AppSwitching.js

--- Angular_Bootstrap_AppSwitching.js
+++ Angular_Bootstrap_AppSwitching.js
@@ -11,6 +11,12 @@
 function initAngular(app_name){
     var _el = "[data-application='"+app_name+"']";
     var dom_el = $(_el).get(0);
+
+    var rootscope = angular.element(dom_el).scope();
+    if (rootscope) {
+        rootscope.$destroy();
+    }
+
     console.log(dom_el);
     angular.element(function() {
         angular.bootstrap(dom_el, [app_name]);
